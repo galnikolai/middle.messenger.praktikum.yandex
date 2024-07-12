63509b720ebeb1ad41ad21e1bb5e2e50235884af
galnikolai <nicholas@markeaze.com>
2024-07-13 02:11:47 +0300

    first sprint

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+import "./pages/chats/chats.css";
+import "./pages/errors/errors.css";
+import "./pages/settings/profile/profile.css";
+import "./pages/settings/password/password.css";
+import "./pages/login/login.css";
+import "./pages/signin/signin.css";
+
+import { router } from "./router";
+
+document.addEventListener("DOMContentLoaded", () => {
+  router();
+});
