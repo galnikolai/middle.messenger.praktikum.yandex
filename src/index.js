3b01945a62b4de08bc1457f32955108bb6b4cbb7
galnikolai <nicholas@markeaze.com>
2024-07-13 04:23:39 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./styles/main.css";
 import "./pages/chats/chats.css";
 import "./pages/errors/errors.css";
 import "./pages/settings/profile/profile.css";
-import "./pages/settings/password/password.css";
 import "./pages/login/login.css";
 import "./pages/signin/signin.css";
 
