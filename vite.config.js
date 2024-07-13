6776e8219985b4522b2a6a5495f9d36b900fe66b
galnikolai <nicholas@markeaze.com>
2024-07-13 05:14:14 +0300

    some more

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,17 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve("src/index.html"),
-        signin: resolve("src/pages/signin/signin.html"),
-        login: resolve("src/pages/login/login.html"),
-        404: resolve("src/pages/errors/404.html"),
-        500: resolve("src/pages/errors/500.html"),
-        profile: resolve("src/pages/settings/profile/profile.html"),
-        password: resolve("src/pages/settings/password/password.html"),
-        chats: resolve("src/pages/chats/chats.html"),
+        main: resolve(__dirname, "src/index.html"),
+        signin: resolve(__dirname, "src/pages/signin/signin.html"),
+        login: resolve(__dirname, "src/pages/login/login.html"),
+        404: resolve(__dirname, "src/pages/errors/404.html"),
+        500: resolve(__dirname, "src/pages/errors/500.html"),
+        profile: resolve(__dirname, "src/pages/settings/profile/profile.html"),
+        password: resolve(
+          __dirname,
+          "src/pages/settings/password/password.html"
+        ),
+        chats: resolve(__dirname, "src/pages/chats/chats.html"),
       },
     },
   },
