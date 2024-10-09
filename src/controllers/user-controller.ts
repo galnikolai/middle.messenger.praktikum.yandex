52cb7b039aef7d34177379d29ac77561b4056b63
galnikolai <nicholas@markeaze.com>
2024-10-09 05:10:59 +0300

    ggggvdfvdf

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -67,7 +67,7 @@ export class UserController {
         store.set('users', response)
       })
       .catch((error: any) => {
-        console.log('Error caught in promise chain:', error.message)
+        console.log('Error cau ght in promise chain:', error.message)
       })
   }
 
