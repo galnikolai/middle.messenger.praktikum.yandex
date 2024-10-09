e60d89048ed989baf060f9a5af5e865c7cf1d865
galnikolai <nicholas@markeaze.com>
2024-10-09 03:54:25 +0300

    fvhjdnfvdf

---
+++ src/modules/HttpsTransport.spec.ts
@@ -0,0 +1,14 @@
+describe('Апи', () => {
+  // it('Использует маршрурты', function () {
+  //   this.router.go(Routes.Messenger)
+  //   expect(this.router.history.length).to.have.lengthOf(1)
+  // })
+  // it('Переход на новую страницу должен менять состояние сущности history', () => {
+  //   // router.go(Routes.LogIn)
+  //   // router.go(Routes.SignUp)
+  //   // router.go(Routes.Messenger)
+  //   // router.go(Routes.Settings)
+  //   // router.go(Routes.SettingsPassword)
+  //   expect(window.history.length).to.eq(6)
+  // })
+})
